00bef058d51e079597dbe4b496540665bf588d26
miguel23-Rojas <68246138+miguel23-Rojas@users.noreply.github.com>
2021-03-17 11:51:19 -0600

    Create Actividad.php

--- app/Models/Actividad.php
+++ app/Models/Actividad.php
@@ -3,37 +3,26 @@ namespace App\Models;
 
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 /**
-* Descripción: Modelo para entregar de actividad del usuario 
+* Descripción: Modelo para administracion de entregar de actividad del usuario 
 * Autor: Miguel Arturo Rojas Hernández
 * Fecha: 17/03/2021
-*
-* Descripción: Modelo para calificar actividad y realizar retroalimentación
-* Fecha: 17/03/2021
-* Autor: Balderas Zamora josé Guillermo
-*
 */
 
-class Registro extends Eloquent{
+class Actividad extends Eloquent{
 	protected $connection = 'mongodb';
-    
-  
-
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $collection = 'actividad';
+    protected $primaryKey = 'Id_actividad';
     protected $fillable = [
-        'idActividad',
-        'estatusCalificación',
-        'nombreActividad',
-        'estatusEntrega',
-        'ultimaModificacion',
-        'fechaEntrega',
-        'tiempoRestante',
-        'fechaEnvio',
-        'retroAlimentacion',
+        'Id_actividad',
+        'Estatus_calificacion',
+        'Nombre_actividad',
+        'Estatus_entrega',
+        'Ultima_modificacion',
+        'Fecha_entrega',
+        'Tiempo_restante',
+        'Fecha_envio',
+        'Retro_alimentacion',
         'calificacion'
+        
     ];
 }
