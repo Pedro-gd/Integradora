00d1105b5328ba83869bf06f8429e162c1a87fe9
Guillermo Balderas <72248396+MemoAllen@users.noreply.github.com>
2021-03-17 11:47:12 -0600

    Update Actividad.php

--- app/Models/Actividad.php
+++ app/Models/Actividad.php
@@ -6,6 +6,11 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 * Descripción: Modelo para entregar de actividad del usuario 
 * Autor: Miguel Arturo Rojas Hernández
 * Fecha: 17/03/2021
+*
+* Descripción: Modelo para calificar actividad y realizar retroalimentación
+* Fecha: 17/03/2021
+* Autor: Balderas Zamora josé Guillermo
+*
 */
 
 class Registro extends Eloquent{
